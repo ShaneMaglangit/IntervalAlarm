13d68aabb72f90db54918c4e3192aa48523e3e87
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-02-24 23:53:45 +0800

    Added Data Binding To Alarm List Items

--- app/src/main/java/com/shanemaglangit/intervalalarm/util/BindingAdapters.kt
+++ app/src/main/java/com/shanemaglangit/intervalalarm/util/BindingAdapters.kt
@@ -11,3 +11,25 @@ fun setTime(view: TextView, value: Long?) {
     if(value != null) calendar.timeInMillis = value
     view.text = SimpleDateFormat("hh : mm a").format(calendar.time).toUpperCase()
 }
+
+@BindingAdapter("startTime", "endTime")
+fun setListTime(view: TextView, startTime: Long, endTime: Long) {
+    val calendar = Calendar.getInstance()
+    val start: String
+    val end: String
+    calendar.timeInMillis = startTime
+    start = SimpleDateFormat("hh:mm a").format(calendar.time).toUpperCase()
+    calendar.timeInMillis = endTime
+    end = SimpleDateFormat("hh:mm a").format(calendar.time).toUpperCase()
+    view.text = "$start to $end"
+}
+
+@BindingAdapter("android:text")
+fun setText(view: TextView, value: Int) {
+    view.text = value.toString()
+}
+
+//@BindingAdapter("listInterval")
+//fun setListInterval(view: TextView, value: Int) {
+//    view.text = value.toString()
+//}
